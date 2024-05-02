8f76e36f4616613f0e250ac7ccb723e2e8ac9962
DimaFront12 <165731696+DimaFront12@users.noreply.github.com>
2024-05-03 01:21:23 +0300

    validation with React Hook Form & Yup

--- src/components/Form/WindowWithForm.jsx
+++ src/components/Form/WindowWithForm.jsx
@@ -1,18 +1,47 @@
 import Styles from './WindowWithForm.module.css';
 import { useRef, useState } from 'react';
-import { useStore } from '../../store/useStore';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
+
+const fieldsSchema = yup.object().shape({
+	email: yup.string()
+		.matches(
+			/^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i,
+			'Некорректно введена почта. При вводе почты должны быть символы "@" и "."',
+		)
+		.required('Поле обязательно для заполнения'),
+	password: yup.string()
+		.min(9, 'Пароль должен быть больше 8 символов')
+		.required('Поле обязательно для заполнения'),
+	repeatPassword: yup.string()
+		.required('Поле обязательно для заполнения')
+		.oneOf([yup.ref('password')], 'Пароли не совпадают'),
+});
 
 export const WindowWithForm = () => {
-	const { getState, updateState } = useStore();
-	const { email, password, repeatPassword } = getState();
-	const [errorEmail, setErrorEmail] = useState('');
-	const [errorPassword, setErrorPassword] = useState('');
-	const [errorRepeatPassword, setErrorRepeatPassword] = useState('');
-	const [errorSubmit, setErrorSubmit] = useState('');
 	const [typeInputPass, setTypeInputPass] = useState('password');
 	const [typeInputRepPass, setTypeInputRepPass] = useState('password');
 
 	const submitFormRef = useRef(null);
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		defaultValues: {
+			email: '',
+			password: '',
+			repeatPassword: '',
+		},
+		resolver: yupResolver(fieldsSchema),
+	})
+
+	const errorEmail = errors.email?.message;
+	const errorPassword = errors.passwordg?.message;
+	const errorRepeatPassword = errors.repeatPassword?.message;
+
 
 	const changeTypeInputPass = (evt) => {
 		evt.preventDefault();
@@ -24,76 +53,32 @@ export const WindowWithForm = () => {
 		setTypeInputRepPass(typeInputRepPass === 'password' ? 'text' : 'password');
 	};
 
-	const onSubmit = (evt) => {
-		evt.preventDefault();
-
-		if (!email || !password || !repeatPassword) {
-			setErrorSubmit('Не все поля ввода заполнены');
-		} else {
-			setErrorSubmit('');
-			console.log(getState());
-		}
+	const getData = (formData) => {
+		console.log(formData)
 	};
 
 	const onChange = ({ target }) => {
-		updateState(target.name, target.value);
-
-		if (password.length > 8) {
-			setErrorPassword(null);
+		if (
+			!errorEmail &&
+			!errorPassword &&
+			target.value === "1"
+		) {
+			submitFormRef.current.focus();
 		}
-
-		if (target.name === "repeatPassword" && !errorEmail && !errorPassword && target.value === password) {
-			submitFormRef.current.focus()
-		}
-	};
-
-	const onEmailBlur = ({ target }) => {
-		let newError = null;
-
-		const emailRegExp = /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i;
-
-		if (!emailRegExp.test(target.value)) {
-			newError =
-				'Некорректно введена почта. При вводе почты должны быть символы "@" и "."';
-		}
-
-		setErrorEmail(newError);
-	};
-
-	const onPasswordBlur = () => {
-		let newError = null;
-
-		if (password.length <= 8) {
-			newError = 'Пароль должен быть больше 8 символов';
-		}
-
-		setErrorPassword(newError);
-	};
-
-	const onRepeatPasswordBlur = ({ target }) => {
-		let newError = null;
-
-		if (target.value !== password) {
-			newError = 'Пароли не совпадают';
-		}
-
-		setErrorRepeatPassword(newError);
 	};
 
 	return (
 		<div className={Styles.rectangle}>
 			<p className={Styles.register}>Регистрация</p>
-			<form className={Styles['reg-form']} onSubmit={onSubmit}>
+			<form className={Styles['reg-form']} onSubmit={handleSubmit(getData)}>
 				<label className={Styles['reg-form__label']}>
 					<p className={Styles['reg-form__text']}>Почта</p>
 					<input
 						className={Styles['reg-form__email']}
 						name="email"
-						value={email}
 						type="email"
 						placeholder="Почта"
-						onChange={onChange}
-						onBlur={onEmailBlur}
+						{...register("email")}
 					></input>
 					{errorEmail && (
 						<p className={Styles['reg-form__error']}>{errorEmail}</p>
@@ -105,11 +90,9 @@ export const WindowWithForm = () => {
 						<input
 							className={Styles['reg-form__password']}
 							name="password"
-							value={password}
 							type={typeInputPass}
 							placeholder="Пароль"
-							onChange={onChange}
-							onBlur={onPasswordBlur}
+							{...register("password")}
 						></input>
 						<button
 							className={Styles['reg-form__show']}
@@ -126,11 +109,10 @@ export const WindowWithForm = () => {
 						<input
 							className={Styles['reg-form__password']}
 							name="repeatPassword"
-							value={repeatPassword}
 							type={typeInputRepPass}
 							placeholder="Повторите пароль"
 							onChange={onChange}
-							onBlur={onRepeatPasswordBlur}
+							{...register("repeatPassword")}
 						></input>
 						<button
 							className={Styles['reg-form__show']}
@@ -141,7 +123,6 @@ export const WindowWithForm = () => {
 						<p className={Styles['reg-form__error']}>{errorRepeatPassword}</p>
 					)}
 				</label>
-
 				<button
 					ref={submitFormRef}
 					type="submit"
@@ -150,9 +131,6 @@ export const WindowWithForm = () => {
 				>
 					Зарегистрироваться
 				</button>
-				{errorSubmit && (
-					<p className={Styles['reg-form__error-submit']}>{errorSubmit}</p>
-				)}
 			</form>
 		</div>
 	);
